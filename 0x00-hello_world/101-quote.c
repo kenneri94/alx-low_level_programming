b55b1ad471c8801f6b97453da46ac708bd93144e
kenneri94e <kenneri94@gmail.com>
2023-06-25 11:41:05 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
-{ #include <stdio.h>
-
-int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
-
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 success
+ */
+int main(void)
+{write
+(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return (1);
-} }
+}
+
