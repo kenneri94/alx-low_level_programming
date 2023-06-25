8a784b3cdb87bdbaeea7bce6df0b8927a4a998ea
kenneri94e <kenneri94@gmail.com>
2023-06-25 06:56:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactlyi
- *
- * Return: Always 0 (Success)
+ * Description: main - prints ut a string to stdout.
+ * Return: 0 if success
  */
-int main(void)
-{ puts("Programming is like building a multilingual puzzle\n");
-return (0);
+ int main(void)
+ {
+ puts("\"programming is like building a multilingual puzzle");
+ return (0);
+ }
