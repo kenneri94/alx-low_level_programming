4a14d833ee40406439914277b5e568bb8ac920ff
kenneri94e <kenneri94@gmail.com>
2023-06-26 20:07:13 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int length, z, half;
 	char temp;
 
-	for (length = 0; s[length] != '\0'; length++);
+	for (length = 0; s[length] != '\0'; length++)
+		;
 	z = 0;
 	half = length / 2;
-
 	while (half--)
 	{
 		temp = s[length - z - 1];
