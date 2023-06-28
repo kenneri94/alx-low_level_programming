5a0efcb5dc6d5e5f342812ed899275bb1c6e151d
kenneri94e <kenneri94@gmail.com>
2023-06-28 16:51:17 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,19 @@
  */
 int main(void)
 {
-	int lowerCase = 'a';
-	while (lowerCase <= 'z')
-
-	{
-		if (lowerCase == 'e' || lowerCase == 'q')
-		{
-			lowerCase += 1;
-		}
-		else
-		{
-			putchar(lowerCase);
-			lowerCase += 1;
-		}
-	}
-	putchar('\n'):
-		return (0);
-}                        
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if (lowerCase == 'e' || lowerCase == 'q')
+{
+lowerCase += 1;
+}
+else
+{
+putchar(lowerCase);
+lowerCase += 1;
+}
+}
+putchar('\n');
+return (0);
+}
