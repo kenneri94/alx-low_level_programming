cddeae80b1fc5f3f5587bc57bf7bc78154bd676b
kenneri94e <kenneri94@gmail.com>
2023-07-26 07:13:36 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -5,7 +5,8 @@
  * @separator: Separates between strings
  * @n: number of arguments
  */
-void print_strings(const char *separator, const unsigned int n, ...){
+void print_strings(const char *separator, const unsigned int n, ...)
+{
 	unsigned int i;
 	char *str;
 
