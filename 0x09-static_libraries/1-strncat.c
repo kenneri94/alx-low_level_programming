b40dcc431453b5bb5596bd18112c5622463da62f
kenneri94 <kenneri94@gmail.com>
2023-09-06 17:23:53 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,33 +1,26 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
- * @dest: input value
- * @src: input value
- * @n: input value
- *
- * Return: dest
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int count = 0, count2 = 0;
 
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
-	i = 0;
-	while (dest[i] != '\0')
+	while (count2 >= 0)
 	{
-		i++;
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
 	}
-	j = 0;
-	while (j < n && src[j] != '\0')
-	{
-	dest[i] = src[j];
-	i++;
-	j++;
-	}
-	dest[i] = '\0';
 	return (dest);
 }
- 
-
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,15 +1,24 @@
 #include "main.h"
-		
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
- 
- 
- 
 
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ *
+ *@c: The int to print
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+
+{
+
+if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
