a6f6981d7e540d44d911978028df39a1736eb75f
kenneri94 <kenneri94@gmail.com>
2023-10-15 23:20:59 -0700

    revised

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcnt1.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
