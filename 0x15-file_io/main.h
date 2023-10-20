8f35a624ce3ab2e5fa9fb224de478e12f20da483
kenneri94 <kenneri94@gmail.com>
2023-10-20 15:17:25 -0700

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <fcnt1.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
