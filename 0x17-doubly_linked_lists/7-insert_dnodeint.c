b1004024ec3b3fcfb0dd1b2bfdc524973acfc803
kenneri94 <kenneri94@gmail.com>
2024-08-14 18:05:09 -0700

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	for (count = 0; count < idx; count++)
+	for (count = 1; count < idx; count++)
 	{
 		if (temp == NULL)
 		return (NULL);
